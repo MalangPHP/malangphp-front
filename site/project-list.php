f4ad6c1e0cc46812c589480fb68132ffb5f24655
Yuri Pratama <yuripertamax@gmail.com>
2015-07-01 20:14:47 +0700

    Init project list page

---
+++ site/project-list.php
@@ -0,0 +1,158 @@
+<?php include_once('../template/head.php') ?>
+<?php include_once('../template/header.php') ?>
+
+<section class="page-title aleft">
+    <div class="mask mask-purple">&nbsp;</div>
+        <div class="container">
+            <div class="col-md-6">
+                <h3>Open Source Projects</h3>
+                <small>Lorem Ipsum Dolor Sit Amet</small>
+            </div>
+            <div class="col-md-6 aright btn-area">
+                <a href="" class="active">Released</a>
+                <a href="">Development</a>
+            </div>
+        </div>
+</section>
+
+
+<section class="content row-fluid purple" id="project">
+    <div class="container">
+        <div class="col-md-9" id="mainbar">            
+            
+            <h3 class="subtitle">
+                Project Directories
+                <!-- <div class="pull-right">
+                    asd
+                    <form action="">
+                        
+                    <select class="form-control" name="" id="">
+                        <option value="">PHP</option>
+                        <option value="">Javascript</option>
+                        <option value="">Other</option>
+                    </select>
+                    </form>
+                </div> -->
+            </h3>
+
+            <ul class="list-group">
+                <li class="list-group-item">
+                    <div class="pull-left">
+                        <span class="title">
+                            Cras justo odio
+                        </span>
+                        <br>
+                        <span class="desc">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </span>
+                    </div>
+                    <div class="pull-right">
+                        <ul class="btn-area">
+                            <li><a href="" class="github" data-toggle="tooltip" data-placement="top" title="Github Repo Page"><i class="fa fa-github"></i> Github Repo</a></li>
+                            <li><a href="" class="demo"><i class="fa fa-eye"></i> View Demo</a></li>
+                            <li><a href="" class="download"><i class="fa fa-download"></i> Download</a></li>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                </li>
+                <li class="list-group-item">
+                    <div class="pull-left">
+                        <span class="title">
+                            Cras justo odio
+                        </span>
+                        <br>
+                        <span class="desc">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </span>
+                    </div>
+                    <div class="pull-right">
+                        <ul class="btn-area">
+                            <li><a href="" class="github" data-toggle="tooltip" data-placement="top" title="Github Repo Page"><i class="fa fa-github"></i> Github Repo</a></li>
+                            <li><a href="" class="demo"><i class="fa fa-eye"></i> View Demo</a></li>
+                            <li><a href="" class="download"><i class="fa fa-download"></i> Download</a></li>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                </li>
+                <li class="list-group-item">
+                    <div class="pull-left">
+                        <span class="title">
+                            Cras justo odio
+                        </span>
+                        <br>
+                        <span class="desc">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </span>
+                    </div>
+                    <div class="pull-right">
+                        <ul class="btn-area">
+                            <li><a href="" class="github" data-toggle="tooltip" data-placement="top" title="Github Repo Page"><i class="fa fa-github"></i> Github Repo</a></li>
+                            <li><a href="" class="demo"><i class="fa fa-eye"></i> View Demo</a></li>
+                            <li><a href="" class="download"><i class="fa fa-download"></i> Download</a></li>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                </li>
+                <li class="list-group-item">
+                    <div class="pull-left">
+                        <span class="title">
+                            Cras justo odio
+                        </span>
+                        <br>
+                        <span class="desc">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </span>
+                    </div>
+                    <div class="pull-right">
+                        <ul class="btn-area">
+                            <li><a href="" class="github" data-toggle="tooltip" data-placement="top" title="Github Repo Page"><i class="fa fa-github"></i> Github Repo</a></li>
+                            <li><a href="" class="demo"><i class="fa fa-eye"></i> View Demo</a></li>
+                            <li><a href="" class="download"><i class="fa fa-download"></i> Download</a></li>
+                        </ul>
+                    </div>
+                    <div class="clearfix"></div>
+                </li>
+            </ul>
+            
+
+            <ul class="pagination pull-right">
+                <li class="first"><a href="">First</a></li>
+                <li class="prev"><a href="">Prev</a></li>
+                <li><a href="">1</a></li>
+                <li class="active"><a href="">2</a></li>
+                <li><a href="">3</a></li>
+                <li class="next"><a href="">Prev</a></li>
+                <li class="last"><a href="">Last</a></li>
+            </ul>
+
+        </div>
+        <!-- <div class="col-md-1"></div> -->
+        <div class="col-md-3" id="sidebar">
+            <h3 class="subtitle">Language Filter</h3>
+            <ul>
+                <li>
+                    <input type="checkbox"/> <a href="">PHP</a><br>
+                    <small>10 Projects</small>
+                </li>
+                <li>
+                    <a href="">Javascript</a>
+                    <br>
+                    <small>4 Projects</small>
+                </li>
+                <li>
+                    <a href="">Other</a>
+                    <br>
+                    <small>0 Projects</small>
+                </li>
+                <li>
+                    <a href="" class="btn btn-trans-purple">Filter</a>
+                </li>
+            </ul>
+
+            
+
+        </div>   
+    </div>
+</section>
+
+<?php include_once('../template/footer.php') ?>
