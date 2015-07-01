44cee4ff1a6a5b59a93f35f4787dee55986b7516
Yuri Pratama <yuripertamax@gmail.com>
2015-07-01 20:14:32 +0700

    Init blog entry page

---
+++ site/blog-detail.php
@@ -0,0 +1,104 @@
+<?php include_once('../template/head.php') ?>
+<?php include_once('../template/header.php') ?>
+
+<section class="page-title aleft">
+    <div class="mask mask-purple">&nbsp;</div>
+        <div class="container">
+            <div class="col-md-6">
+                <h3>News & Blog</h3>
+                <small>Lorem Ipsum Dolor Sit Amet</small>
+            </div>
+            <div class="col-md-6 aright btn-area">
+                <a href="" class="active">Back to List</a>
+            </div>
+        </div>
+</section>
+
+
+<section class="content row-fluid purple" id="post">
+    <div class="container">
+        <div class="col-md-7" id="mainbar">            
+            
+            <ul>
+                <li>
+                    <h3 class="title">PHP 7.0.0 Alpha 1 Released</h3>
+                    <div class="meta-pre">
+                        <span class="date">
+                            <i class="fa fa-calendar"></i> June 27, 2015
+                        </span>
+                        <span class="time">
+                            <i class="fa fa-clock-o"></i> 08:00
+                        </span>
+                        <span class="place">
+                            <i class="fa fa-user"></i> Prasetyo Wicaksono
+                        </span>
+                        <span class="comment">
+                            <a href="">
+                                
+                            <i class="fa fa-comment"></i> 3 Comments
+                            </a>
+                        </span>
+
+                        <!-- <div class="pull-right aright rtrim-padding">
+                            <span class="speakers">
+                                <i class="fa fa-bullhorn"></i> Speakers: <a href="">Yuri Pratama</a>
+                            </span>
+                            <span class="attendant">
+                                <i class="fa fa-users"></i> Current Attendance: <a href="">10 Persons</a>
+                            </span>
+                        </div> -->
+                    </div>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus expedita dolorem atque enim dolores numquam repudiandae doloribus doloremque mollitia corporis, distinctio odit iste iure quas architecto temporibus a sit minus.</p>
+                    <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus expedita dolorem atque enim dolores numquam repudiandae doloribus doloremque mollitia corporis, distinctio odit iste iure quas architecto temporibus a sit minus.</p>
+                    <div class="meta-post pull-left">
+                        <span class="detail">
+                            <i class="fa fa-share"></i> <a href="">Share</a>
+                        </span>
+                    </div>
+                    <div class="clearfix"></div>
+                </li>
+
+                
+                
+            </ul>
+
+            <ul class="pagination pull-right">
+                <li class="first"><a href="">First</a></li>
+                <li class="prev"><a href="">Prev</a></li>
+                <li><a href="">1</a></li>
+                <li class="active"><a href="">2</a></li>
+                <li><a href="">3</a></li>
+                <li class="next"><a href="">Prev</a></li>
+                <li class="last"><a href="">Last</a></li>
+            </ul>
+
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-4" id="sidebar">
+
+            <h3 class="subtitle">Similar Posts</h3>
+            <ul>
+                <li>
+                    <a href="">Lorem Ipsum Released!</a>
+                    <br>
+                    <small>July 1, 2015</small>
+                </li>
+                <li>
+                    <a href="">Lorem Ipsum Released!</a>
+                    <br>
+                    <small>July 1, 2015</small>
+                </li>
+                <li>
+                    <a href="">Lorem Ipsum Released!</a>
+                    <br>
+                    <small>July 1, 2015</small>
+                </li>
+            </ul>
+
+            
+
+        </div>   
+    </div>
+</section>
+
+<?php include_once('../template/footer.php') ?>
