f072c174ae10333ef1e19ba53a9ae027d86112f6
Yuri Pratama <yuripertamax@gmail.com>
2015-07-01 16:15:08 +0700

    Add FAQ page

---
+++ site/faq.php
@@ -0,0 +1,49 @@
+<?php include_once('../template/head.php') ?>
+<?php include_once('../template/header.php') ?>
+
+<section class="page-title aleft">
+    <div class="mask mask-purple">&nbsp;</div>
+        <div class="container">
+            <div class="col-md-6">
+                <h3>Frequently Ask & Question</h3>
+                <small>Lorem Ipsum Dolor Sit Amet</small>
+            </div>
+            <div class="col-md-6 aright btn-area">
+                <a href="">About Us</a>
+                <a href="">Contact Us</a>
+                <a href="" class="active">FAQ</a>
+            </div>
+        </div>
+</section>
+
+
+<section class="content row-fluid" id="faq">
+    <div class="container">
+        <!-- <div class="alert alert-info">This Page is Under Construction</div> -->
+
+        <div class="col-md-8">
+            <h3 class="subtitle">Have some question? Maybe here you will found </h3>
+            
+            <div id="question-stack">
+                
+            <h3 class="question">Lorem Ipsum?</h3>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt animi accusantium reprehenderit? Recusandae a reprehenderit similique. Ullam ex consectetur in, aliquid illo hic eligendi veniam, fugit, quidem optio ab, atque.</p>
+
+            <h3 class="question">Lorem Ipsum?</h3>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt animi accusantium reprehenderit? Recusandae a reprehenderit similique. Ullam ex consectetur in, aliquid illo hic eligendi veniam, fugit, quidem optio ab, atque.</p>
+
+            <h3 class="question">Lorem Ipsum?</h3>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt animi accusantium reprehenderit? Recusandae a reprehenderit similique. Ullam ex consectetur in, aliquid illo hic eligendi veniam, fugit, quidem optio ab, atque.</p>
+
+            <h3 class="question">Lorem Ipsum?</h3>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt animi accusantium reprehenderit? Recusandae a reprehenderit similique. Ullam ex consectetur in, aliquid illo hic eligendi veniam, fugit, quidem optio ab, atque.</p>
+            
+            </div>
+        </div>
+        <div class="col-md-4" id="sidebar">
+            
+        </div>
+    </div>
+</section>
+
+<?php include_once('../template/footer.php') ?>
