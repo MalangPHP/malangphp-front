1430a4cb25cdd1e070ec0064b23858fffe057708
Yuri Pratama <yuripertamax@gmail.com>
2015-07-01 16:14:38 +0700

    Renaming contact file

--- site/contact.php
+++ site/contact.php
@@ -5,12 +5,12 @@
     <div class="mask mask-purple">&nbsp;</div>
         <div class="container">
             <div class="col-md-6">
-                <h3>About Us</h3>
+                <h3>Contact Us</h3>
                 <small>Lorem Ipsum Dolor Sit Amet</small>
             </div>
             <div class="col-md-6 aright btn-area">
-                <a href="" class="active">About Us</a>
-                <a href="">Contact Us</a>
+                <a href="">About Us</a>
+                <a href="" class="active">Contact Us</a>
                 <a href="">FAQ</a>
             </div>
         </div>
@@ -19,7 +19,37 @@
 
 <section class="content row-fluid" id="contact">
     <div class="container">
-        <div class="alert alert-info">This Page is Under Construction</div>
+        <!-- <div class="alert alert-info">This Page is Under Construction</div> -->
+
+        <div class="col-md-8">
+            <h3 class="subtitle">Fill the form to get in touch with us</h3>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt animi accusantium reprehenderit? Recusandae a reprehenderit similique. Ullam ex consectetur in, aliquid illo hic eligendi veniam, fugit, quidem optio ab, atque.</p>
+            <form action="">
+                <div class="form-group">
+                    <label for="">Name</label>
+                    <input type="text" class="form-control" placeholder="What's your name?">
+                </div>
+                <div class="form-group">
+                    <label for="">Email</label>
+                    <input type="text" class="form-control" placeholder="So we can get back in touch with you">
+                </div>
+                <div class="form-group">
+                    <label for="">Subject</label>
+                    <input type="text" class="form-control" placeholder="What's your purpose?">
+                </div>
+                <div class="form-group">
+                    <label for="">Messages</label>
+                    <textarea class="form-control">Give us some details</textarea>
+                </div>
+                <div class="form-group aright">
+                    <a href="" class="btn btn-lg btn-trans-purple"><i class="fa fa-long-arrow-left"></i> Cancel</a>
+                    <a href="" class="btn btn-lg btn-purple"><i class="fa fa-paper-plane"></i> Submit</a>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-4" id="sidebar">
+            
+        </div>
     </div>
 </section>
 
