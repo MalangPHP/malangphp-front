7feec45f92eea89e6874f357bb3c6c2b7656d030
Yuri Pratama <yuripertamax@gmail.com>
2015-07-01 16:14:55 +0700

    Add about page

---
+++ site/about.php
@@ -0,0 +1,69 @@
+<?php include_once('../template/head.php') ?>
+<?php include_once('../template/header.php') ?>
+
+<section class="page-title aleft">
+    <div class="mask mask-purple">&nbsp;</div>
+        <div class="container">
+            <div class="col-md-6">
+                <h3>About Us</h3>
+                <small>Lorem Ipsum Dolor Sit Amet</small>
+            </div>
+            <div class="col-md-6 aright btn-area">
+                <a href="" class="active">About Us</a>
+                <a href="">Contact Us</a>
+                <a href="">FAQ</a>
+            </div>
+        </div>
+</section>
+
+
+<section class="content row-fluid" id="about">
+    <div class="container">
+        <!-- <div class="alert alert-info">This Page is Under Construction</div> -->
+
+        <div class="col-md-8">
+            <h3 class="subtitle">Who Are We?</h3>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet reprehenderit vel aperiam consectetur quo consequuntur maxime quasi! Aspernatur, aliquid, nemo at quaerat similique quisquam hic beatae deserunt vero, quibusdam veniam.</p>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet reprehenderit vel aperiam consectetur quo consequuntur maxime quasi! Aspernatur, aliquid, nemo at quaerat similique quisquam hic beatae deserunt vero, quibusdam veniam.</p>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet reprehenderit vel aperiam consectetur quo consequuntur maxime quasi! Aspernatur, aliquid, nemo at quaerat similique quisquam hic beatae deserunt vero, quibusdam veniam.</p>
+        </div>
+        <div class="col-md-4" id="sidebar">
+            <h3 class="subtitle">Find Us</h3>
+            <ul>
+                <li><i class="fa fa-home"></i> Malang Digital Lounge</li>
+                <li><i class="fa fa-map-marker"></i> Jalan Basuki Rahmat</li>
+                <li><i class="fa fa-telephone"></i> +628881234567</li>
+                <li><i class="fa fa-paper-plane"></i> contact@malangphp.org</li>
+            </ul>
+            <div id="maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3951.1826838381016!2d112.6301096!3d-7.980060600000009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sid!4v1435740148501" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+
+            <h3 class="subtitle">Social Media</h3>
+            <ul>
+                <li>
+                    <a href="">
+                        <i class="fa fa-facebook"></i> Facebook
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fa fa-twitter"></i> Twitter
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fa fa-github"></i> Github
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fa fa-slack"></i> Slack Channel
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<?php include_once('../template/footer.php') ?>
